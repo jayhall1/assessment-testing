6e23d2b379b0dc88534e9c735575e9e936098308
jaylen  hall <jaylenhall1@gmail.com>
2021-11-19 13:23:30 -0600

    on my heroku part

---
+++ functions.test.ts
@@ -0,0 +1,11 @@
+const {shuffleArray} = require('./utils')
+
+describe('shuffleArray should', () => {
+    test("check to see if shuffle array returns array", () => {
+        expect(shuffleArray).toBe(shuffleArray);
+    })
+    test('items are in the array', () => {
+        expect(shuffleArray)
+        console.log(shuffleArray);
+    })
+})
